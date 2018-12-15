a0afab747884f6a07cf8315c51c9770ce97be5dd
danursin <danursin@gmail.com>
2018-12-14 21:00:43 -0600

    static

--- server.ts
+++ server.ts
@@ -2,7 +2,7 @@ import * as express from "express";
 
 const app: express.Express = express();
 
-app.use("/public", express.static("public"));
+app.use(express.static("public"));
 
 app.get("*", (_req: express.Request, res: express.Response) => res.sendFile(__dirname + "/public/index.html"));
 
